065d66f8e3dc1cf03b4a885a33d6c197071950e4
WilderNube <wtarazonac@gmail.com>
2014-09-03 08:49:27 -0500

    w

--- BullardEncuestas.Core/BL/ReporteBL.cs
+++ BullardEncuestas.Core/BL/ReporteBL.cs
@@ -22,47 +22,50 @@ namespace BullardEncuestas.Core.BL
                     {
                         IdEncuesta = x.IdEncuesta,
                         NombreEncuesta = x.NombreEncuesta,
+                        Estado = x.Estado,
                         IdPeriodo = x.IdPeriodo,
                         NombrePeriodo = x.NombrePeriodo,
                         IdGrupo = x.IdGrupoTrabajo,
                         NombreGrupo = x.NombreGrupo,
+                        PromedioGeneral = x.PromedioGeneral
+                        //s
                     }
                     ).ToList();
 
-                var resultSeccion = new List<ReporteSeccionValorDTO>();
-                foreach (var item in result)
-                {
-                    resultSeccion = context.SP_GetEncuestasReporteSecciones(item.IdEncuesta, item.IdPeriodo, item.IdGrupo)
-                    .Select(y => new ReporteSeccionValorDTO
-                    {
-                        IdSeccion = y.IdSeccion,
-                        OrdenSeccion = y.OrdenSeccion,
-                        IdPregunta = y.IdPregunta,
-                        OrdenPregunta = y.IdPregunta,
-                        IdRespuesta = y.IdRespuestas,
-                        IdTipoRespuesta = y.IdTipoRespuesta,
-                        IdOpcionRespuesta = y.IdOpcion,
-                        Valor = y.Valor
-                    }).ToList();
+                //var result = context.SP_GetEncuestasReporte()
+                //    .Select(x => new ReporteDTO
+                //    {
+                //        IdEncuesta = x.IdEncuesta,
+                //        NombreEncuesta = x.NombreEncuesta,
+                //        IdPeriodo = x.IdPeriodo,
+                //        NombrePeriodo = x.NombrePeriodo,
+                //        IdGrupo = x.IdGrupoTrabajo,
+                //        NombreGrupo = x.NombreGrupo,
+                //    }
+                //    ).ToList();
 
-                    item.ListaSeccionProm = resultSeccion;
-                }
+                //var resultSeccion = new List<ReporteSeccionValorDTO>();
+                //foreach (var item in result)
+                //{
+                //    resultSeccion = context.SP_GetEncuestasReporteSecciones(item.IdEncuesta, item.IdPeriodo, item.IdGrupo)
+                //    .Select(y => new ReporteSeccionValorDTO
+                //    {
+                //        IdSeccion = y.IdSeccion,
+                //        OrdenSeccion = y.OrdenSeccion,
+                //        IdPregunta = y.IdPregunta,
+                //        OrdenPregunta = y.IdPregunta,
+                //        IdRespuesta = y.IdRespuestas,
+                //        IdTipoRespuesta = y.IdTipoRespuesta,
+                //        IdOpcionRespuesta = y.IdOpcion,
+                //        Valor = y.Valor
+                //    }).ToList();
 
-                
-                foreach (var item in result)
-                {
-                    int promedioG = 0, promedioE = 0, promedioS = 0;
-                    int nele = item.ListaSeccionProm.Count;
-                    for (int i = 0; i < nele; i++)
-                    {
-                        //Promedio Evaluados
-                        promedioG = 1;
-                        //Promedio Secciones
+                //    item.ListaSeccionProm = resultSeccion;
+                //}
 
-                    }
 
-                }
-                return new List<ReporteDTO>();
+
+                return result;
             }
 
         }
--- BullardEncuestas.Core/DTO/ReporteDTO.cs
+++ BullardEncuestas.Core/DTO/ReporteDTO.cs
@@ -9,17 +9,18 @@ namespace BullardEncuestas.Core.DTO
     [Serializable]
     public class ReporteDTO
     {
-        public int IdPeriodo { get; set; }
-        public string NombrePeriodo { get; set; }
         public int IdEncuesta { get; set; }
         public string NombreEncuesta { get; set; }
+        public bool Estado { get; set; }
+        public int IdPeriodo { get; set; }
+        public string NombrePeriodo { get; set; }
         public int IdGrupo { get; set; }
         public string NombreGrupo { get; set; }
-        public int sec1Promedio { get; set; }
-        public int sec1PromEvaluados { get; set; }
-        public int sec1PromSecciones { get; set; }
+
+        public decimal? PromedioGeneral { get; set; }
         public List<DetalleEvaluadoDTO> ListaEvaluados { get; set; }
         public List<ReporteSeccionValorDTO> ListaSeccionProm { get; set; }
         public ReporteDTO reporteAnterior { get; set; }
+        //s
     }
 }
